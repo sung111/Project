29d10b3c381b2fb09ed6e2549f00f1303e5fe87f
DESKTOP-IJSG3N5\admin <zmddhkddl3211@naver.com>
2025-03-11 14:44:42 +0900

    제발

--- project_java/src/main/java/dto/Performance_DTO.java
+++ project_java/src/main/java/dto/Performance_DTO.java
@@ -1,6 +1,5 @@
 package dto;
 
-import java.sql.Date;
 import java.sql.Timestamp;
 
 //실적테이블
@@ -10,78 +9,53 @@ public class Performance_DTO {
 	String ProductName;
 	int PlanId;
 	int UserId;
-	Timestamp ReportTime1;
-	Date ReportTime;
-	
+	Timestamp ReportTime;
 	String PerformanceComment;
 	int ProductionCount;
-
+	
 	public int getPerformanceId() {
 		return PerformanceId;
 	}
-
 	public void setPerformanceId(int performanceId) {
 		PerformanceId = performanceId;
 	}
-
 	public String getProductName() {
 		return ProductName;
 	}
-
 	public void setProductName(String productName) {
 		ProductName = productName;
 	}
-
 	public int getPlanId() {
 		return PlanId;
 	}
-
 	public void setPlanId(int planId) {
 		PlanId = planId;
 	}
-
 	public int getUserId() {
 		return UserId;
 	}
-
 	public void setUserId(int userId) {
-		this.UserId = userId;
+		UserId = userId;
 	}
-
 	public Timestamp getReportTime() {
 		return ReportTime;
 	}
-
 	public void setReportTime(Timestamp reportTime) {
-		return userId;
-	}
-
-	public void setUserId(int userId) {
-		this.userId = userId;
-	}
-
-	public Date getReportTime() {
-		return ReportTime;
-	}
-
-	public void setReportTime(Date reportTime) {
 		ReportTime = reportTime;
 	}
-
 	public String getPerformanceComment() {
 		return PerformanceComment;
 	}
-
 	public void setPerformanceComment(String performanceComment) {
 		PerformanceComment = performanceComment;
 	}
-
 	public int getProductionCount() {
 		return ProductionCount;
 	}
-
 	public void setProductionCount(int productionCount) {
 		ProductionCount = productionCount;
 	}
+
+	
 
 }
