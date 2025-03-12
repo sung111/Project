1fbe7e7e7dab4f053ec28a9a2f0bc1ed189f8e49
naisha855 <156628341+naisha855@users.noreply.github.com>
2025-03-12 15:07:58 +0900

    2025-03-12.02

--- project_java/src/main/webapp/js/login.js
+++ project_java/src/main/webapp/js/login.js
@@ -2,27 +2,51 @@
 document.addEventListener("DOMContentLoaded", function () {
     const iframe = document.querySelector("iframe");
     const localBar = document.querySelector(".local-bar");
+    // 로그인 상단 화면
+    const loginLayer = document.getElementById("login-layer");
+    const signUpLayer = document.getElementById("signup-layer");
+    const findLayer = document.getElementById("find-layer");
+    // 로그인 하단 버튼
+    const signUpbutton = document.getElementById("sign-up-button");
+    const findNickNameLink = document.getElementById("findid_password-button");
+    const signUpConpleteButton = document.getElementById("sign-up-completed-button");
+    const findAccountButton = document.getElementById("find-account-button");
+    const cencelButton = document.getElementById("cancel-button");
+    // 회원가입 버튼을 누르면 회원가입 창으로 바뀐다.
+    signUpbutton.addEventListener("click", function () {
+        // 상단
+        loginLayer.style.display = "none";
+        findLayer.style.display = "none";
+        signUpLayer.style.display = "flex";
+        // 하단
+        findNickNameLink.style.display = "none";
+        signUpConpleteButton.style.display = "flex";
+        findAccountButton.style.display = "none";
+        cencelButton.style.display = "flex";
+    })
+
+
 
     function LocalNavigationbar(tagname, componantname) {
         localBar.innerHTML = `<li>Home</li> <li>> ${tagname}</li> <li>> ${componantname}</li>`;
     }
 
     const navigationItems = [
-        { selector: "#nav-mainlogo", src: "componant/MainTitle.html", breadcrumb: "Home" },
-        { selector: "#nav-bomlist", src: "componant/standard.html", breadcrumb: "기준관리, 부품코드" },
-        { selector: "#nav-process", src: "componant/production_process.html", breadcrumb: "기준관리, 생산공정" },
-        { selector: "#nav-inspection", src: "componant/Inspection_standards.html", breadcrumb: "기준관리, 검사기준" },
-        { selector: "#nav-bom", src: "componant/bom_v2.html", breadcrumb: "기준관리, BOM" },
-        { selector: "#nav-workorder", src: "componant/WorkOrder.html", breadcrumb: "생산관리, 생산계획" },
-        { selector: "#nav-perfomanceRegistratio", src: "componant/performanceRegistration.html", breadcrumb: "생산관리, 실적등록" },
-        { selector: "#nav-qualityControl", src: "componant/qualityControl.html", breadcrumb: "생산관리, 품질관리" },
-        { selector: "#nav-inventory", src: "componant/01_재고조사현황.html", breadcrumb: "생산관리, 재고현황조회" },
-        { selector: "#nav-perform", src: "componant/perform_0203.html", breadcrumb: "실적보고서, 실적보고서" },
-        { selector: "#nav-report", src: "componant/report.html", breadcrumb: "실적보고서, 주요지표" },
-        { selector: "#nav-chart", src: "componant/chart_0203.html", breadcrumb: "실적보고서, 대시보드" },
-        { selector: "#nav-공지사항", src: "componant/02_공지사항.html", breadcrumb: "게시판, 공지사항" },
-        { selector: "#nav-사내복지", src: "componant/03_사내복지.html", breadcrumb: "게시판, 사내복지" },
-        { selector: "#nav-일반게시판", src: "componant/04_일반게시판.html", breadcrumb: "게시판, 일반게시판" }
+        { selector: "#nav-mainlogo", src: "MainTitle.jsp", breadcrumb: "Home" },
+        { selector: "#nav-bomlist", src: "standard.jsp", breadcrumb: "기준관리, 부품코드" },
+        { selector: "#nav-process", src: "production_process.jsp", breadcrumb: "기준관리, 생산공정" },
+        { selector: "#nav-inspection", src: "Inspection_standards.jsp", breadcrumb: "기준관리, 검사기준" },
+        { selector: "#nav-bom", src: "bom_v2.jsp", breadcrumb: "기준관리, BOM" },
+        { selector: "#nav-workorder", src: "WorkOrder.jsp", breadcrumb: "생산관리, 생산계획" },
+        { selector: "#nav-perfomanceRegistratio", src: "performanceRegistration.jsp", breadcrumb: "생산관리, 실적등록" },
+        { selector: "#nav-qualityControl", src: "qualityControl.jsp", breadcrumb: "생산관리, 품질관리" },
+        { selector: "#nav-inventory", src: "01_재고조사현황.jsp", breadcrumb: "생산관리, 재고현황조회" },
+        { selector: "#nav-perform", src: "perform_0203.jsp", breadcrumb: "실적보고서, 실적보고서" },
+        { selector: "#nav-report", src: "report.jsp", breadcrumb: "실적보고서, 주요지표" },
+        { selector: "#nav-chart", src: "chart_0203.jsp", breadcrumb: "실적보고서, 대시보드" },
+        { selector: "#nav-공지사항", src: "02_공지사항.jsp", breadcrumb: "게시판, 공지사항" },
+        { selector: "#nav-사내복지", src: "03_사내복지.jsp", breadcrumb: "게시판, 사내복지" },
+        { selector: "#nav-일반게시판", src: "04_일반게시판.jsp", breadcrumb: "게시판, 일반게시판" }
     ];
 
     navigationItems.forEach(item => {
