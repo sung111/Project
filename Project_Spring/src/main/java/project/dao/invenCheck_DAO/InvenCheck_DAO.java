b6eea150cc6c7168818027dbe0b28f195f3c4752
DESKTOP-IJSG3N5\admin <zmddhkddl3211@naver.com>
2025-04-16 17:27:29 +0900

    재고현황조회 등록완료, 수정 구현중

--- Project_Spring/src/main/java/project/dao/invenCheck_DAO/InvenCheck_DAO.java
+++ Project_Spring/src/main/java/project/dao/invenCheck_DAO/InvenCheck_DAO.java
@@ -6,14 +6,26 @@ import project.dto.InvenCheck_DTO;
 
 public interface InvenCheck_DAO {
 	
+//	페이징
 	public List page(InvenCheck_DTO dto);
 	public int invenTotalCount();
+	
+//	삭제
 	public int matInvenDelete(InvenCheck_DTO dto);
 	public int productInvenDelete(InvenCheck_DTO dto);
+	
+//	검색
 	public List searchInven(InvenCheck_DTO dto);
 	public int searchInvenCount(InvenCheck_DTO dto);
 	
+//	등록
 	public int matInsertInven(InvenCheck_DTO dto);
+	public int prodInsertInven(InvenCheck_DTO dto);
+	
+//	옵션 리스트
+	public List matDB(InvenCheck_DTO dto);
+	public List prodDB(InvenCheck_DTO dto);
+	
 	
 	
 }
